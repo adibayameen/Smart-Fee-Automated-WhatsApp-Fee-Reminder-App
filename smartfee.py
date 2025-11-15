ae5322379a410c8f99040e9589c99c85d3ad521a
Adiba Yameen <adeebayameen2@gmail.com>
2025-11-16 03:15:03 +0500

    Refactor message sending logic for clarity

--- smartfee.py
+++ smartfee.py
@@ -14,7 +14,7 @@ if upload is not None:
         for i,row in df.iterrows():
             phone=f"+{row['phone']}"
             Fees_status=str(row["fee_status"])
-            if Fees_status == "PAID":
+            if Fees_status.strip().upper() == "PAID":
                 message=("Dear Parents,\n\nThis is to confirm the payment of your child’s school fee. We sincerely appreciate your timely payment and continued support towards the school’s educational objectives.\n\nwarm regards,\nAccount Department ")
                 kit.sendwhatmsg_instantly(phone,message,wait_time=35)
                 time.sleep(8)
@@ -28,4 +28,5 @@ if upload is not None:
                 print("send")
         st.success("All Message Successfully Send")
 st.info("Deveolped by Adeeba Yameen Sheikh")
+
 
